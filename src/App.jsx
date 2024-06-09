b9f2fa06021ca50ead130365f3e5ada189d153b6
Yi Xun <yixun89@live.com>
2024-06-09 17:47:31 +0800

    add new icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoins } from '@fortawesome/free-solid-svg-icons';
 
 import { StockProvider } from './context/StockContext';
 import StockEntryForm from './components/StockEntryForm';
@@ -10,6 +11,7 @@ const App = () => {
 <StockProvider>
       <div className="app-container">
         <header className="header">
+        <FontAwesomeIcon icon={faCoins} size="2x" className="icon"/>
        <h1>Finance Dashboard</h1>
         </header>
         <main className="main-content">
